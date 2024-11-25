d84ef0844649cef60a4b82648cfb2fb40170e581
Jae-Hong-Kim <newfly101@naver.com>
2024-11-26 04:06:42 +0900

    호이스팅

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// console.log("hello");
+
+// 호이스팅
+console.log(greeting);
+
+var greeting = "hello";
+
+// console.log(gretting_let);
+//
+// let gretting_let = "hello";
+
+func();
+
+function func() {
+    console.log(1);
+}
