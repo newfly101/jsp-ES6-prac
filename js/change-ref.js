c64604942983693e140fc565c4ea412f4ab3d028
Jae-Hong-Kim <newfly101@naver.com>
2024-11-27 01:54:29 +0900

    [22] call() method

---
+++ js/change-ref.js
@@ -0,0 +1,14 @@
+const fullName = function () {
+    console.log(`${this.firstName} ${this.lastName}`);
+}
+const fullName2 = function (city, country) {
+    console.log(`${this.firstName} ${this.lastName} ${city} ${country}`);
+}
+const persion1 = {
+    firstName: 'James',
+    lastName: 'January',
+}
+
+fullName(); // undefined undefined
+fullName.call(persion1); // James January
+fullName2.call(persion1, 'Oslo', 'Norway'); // James January Oslo Norway
