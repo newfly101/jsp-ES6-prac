098838ca89751f65aad2cceda0f4d37867b432fa
Jae-Hong-Kim <newfly101@naver.com>
2024-11-28 06:31:28 +0900

    [28] Map, Filter, Reduce

--- js/Destructuring.js
+++ js/Destructuring.js
@@ -11,3 +11,11 @@ const studentDetails = {
 const {firstName: fName = 'not given', lastName} = studentDetails;
 console.log(fName);
 console.log(lastName);
+
+// reducer
+const arr = [0,1,2,3,4];
+let result = arr.reduce( (acc, cur, idx, arr) => {
+    console.log(`합산 : ${acc}, 현재값: ${cur}, index: ${idx}`);
+    return acc + cur;
+})
+console.log(`합산 : ${result}`);
