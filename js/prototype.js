50098b45dc32e9d7bb7f0aec37ed57d85d5f32cf
Jae-Hong-Kim <newfly101@naver.com>
2024-11-29 00:37:53 +0900

    [40] jsp prototype : 프로토타입 지정1

--- js/prototype.js
+++ js/prototype.js
@@ -2,11 +2,11 @@ function Person(name, email, birthday) {
     this.name = name;
     this.email = email;
     this.birthday = new Date(birthday);
-    this.calculateAge = function() {
-        const diff = Date.now() - this.birthday.getTime();
-        const ageDate = new Date(diff);
-        return Math.abs(ageDate.getUTCFullYear() - 1970);
-    }
+}
+Person.prototype.calculateAge = function() {
+    const diff = Date.now() - this.birthday.getTime();
+    const ageDate = new Date(diff);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
 }
 
 const john = new Person('John', 'john@naver.com', '7-10-91');
