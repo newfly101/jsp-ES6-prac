e51c917788aad38c01b10c71e261b6081b0293f5
Jae-Hong-Kim <newfly101@naver.com>
2024-11-29 00:37:10 +0900

    [40] jsp prototype

---
+++ js/prototype.js
@@ -0,0 +1,15 @@
+function Person(name, email, birthday) {
+    this.name = name;
+    this.email = email;
+    this.birthday = new Date(birthday);
+    this.calculateAge = function() {
+        const diff = Date.now() - this.birthday.getTime();
+        const ageDate = new Date(diff);
+        return Math.abs(ageDate.getUTCFullYear() - 1970);
+    }
+}
+
+const john = new Person('John', 'john@naver.com', '7-10-91');
+const han = new Person('han', 'han@naver.com', '22-8-94');
+
+console.log(john);
