d3ad1a4389a57821be30dc89269e695e85d18a11
Jae-Hong-Kim <newfly101@naver.com>
2024-11-29 08:38:07 +0900

    [47] Iterator

--- js/iterator.js
+++ js/iterator.js
@@ -1,19 +1,21 @@
-function makeIterator(numbers) {
-    let nextIndex = 0;
-
-    return {
-        next: function () {
-            return nextIndex < numbers.length ?
-                { value: numbers[nextIndex++], done: false } :
-                { value: undefined, done: true }
-        }
-    }
-}
+// function makeIterator(numbers) {
+//     let nextIndex = 0;
+//
+//     return {
+//         next: function () {
+//             return nextIndex < numbers.length ?
+//                 { value: numbers[nextIndex++], done: false } :
+//                 { value: undefined, done: true }
+//         }
+//     }
+// }
 
 const numberArr = [1,2,3];
-const numberIterator = makeIterator(numberArr);
+// const numberIterator = makeIterator(numberArr);
+const numberIterator = numberArr[Symbol.iterator]();
+// 반복자를 반환하는 메서드 => 이것을 이용해서 반복자를 반환
+console.log(numberIterator.next());
+console.log(numberIterator.next());
+console.log(numberIterator.next());
+console.log(numberIterator.next());
 
-console.log(numberIterator.next());
-console.log(numberIterator.next());
-console.log(numberIterator.next());
-console.log(numberIterator.next());
